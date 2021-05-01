e99acbd0b2a08e602a338a2a265772f185f2aafb
Tyll'a <psduckie@yahoo.com>
2021-05-01 00:38:35 -0400

    Added methods to create new characters

--- Program.cs
+++ Program.cs
@@ -6,6 +6,21 @@ namespace nacs_tracker
     class Program
     {
         static List<Character> charList;
+        static Character AddCharacter(int id, string name, char position, int hp) {
+            Character newChar = new Character();
+            newChar.Id = id;
+            newChar.Name = name;
+            newChar.Position = position;
+            newChar.Hp = hp;
+            return newChar;
+        }
+        static Character AddCharacter(int id, string name, int hp) {
+            Character newChar = new Character();
+            newChar.Id = id;
+            newChar.Name = name;
+            newChar.Hp = hp;
+            return newChar;
+        }        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
