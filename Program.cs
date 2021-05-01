8dc4736a1ef2ca6d32e842ef823e38dfc866298b
Tyll'a <psduckie@yahoo.com>
2021-04-30 23:35:28 -0400

    Created blank project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nacs_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
