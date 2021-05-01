7665af718dcac0cb84c6a3686d23df2ac77eed2b
Tyll'a <psduckie@yahoo.com>
2021-05-01 00:19:31 -0400

    Added character list variable

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace nacs_tracker
 {
     class Program
     {
+        static List<Character> charList;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
