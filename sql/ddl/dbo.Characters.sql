0485d30e024a929a82140e8ffef76429bfc70231
Tyll'a <psduckie@yahoo.com>
2021-07-05 04:55:54 -0400

    Added actions into DB

--- sql/ddl/dbo.Characters.sql
+++ sql/ddl/dbo.Characters.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[Characters]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
     [Name] VARCHAR(20) NOT NULL, 
     [Position] CHAR(1) NULL, 
     [Hp] INT NOT NULL, 
     [MaxHp] INT NOT NULL, 
     [Charge] INT NOT NULL DEFAULT 0, 
     [Defense] INT NOT NULL DEFAULT 0, 
-    [Action] INT NOT NULL, 
-    [Target] INT NOT NULL,
+    [Action] INT NULL, 
+    [Target] INT NULL,
 	CONSTRAINT FK_Target_Character FOREIGN KEY (Target) REFERENCES [dbo].[Characters] (Id),
     CONSTRAINT FK_Action FOREIGN KEY (Action) REFERENCES [dbo].[Actions] (Id)
 )
