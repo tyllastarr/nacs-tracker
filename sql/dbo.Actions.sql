c8671132cfd249465d0bdbfc6d43a2e353e8ea05
Tyll'a <psduckie@yahoo.com>
2021-07-05 00:41:38 -0400

    Added SQL for LocalDB databases

---
+++ sql/dbo.Actions.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[Actions]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Action] VARCHAR(8) NOT NULL
+)
---
+++ sql/dbo.Characters.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Characters]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(20) NOT NULL, 
+    [Position] CHAR(1) NULL, 
+    [Hp] INT NOT NULL, 
+    [MaxHp] INT NOT NULL, 
+    [Charge] INT NOT NULL DEFAULT 0, 
+    [Defense] INT NOT NULL DEFAULT 0, 
+    [Action] INT NOT NULL, 
+    [Target] INT NOT NULL,
+	CONSTRAINT FK_Target_Character FOREIGN KEY (Target) REFERENCES [dbo].[Characters] (Id),
+    CONSTRAINT FK_Action FOREIGN KEY (Action) REFERENCES [dbo].[Actions] (Id)
+)
